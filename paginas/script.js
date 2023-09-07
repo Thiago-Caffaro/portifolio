8320adbad7a806b80d102c640777748fc450abe6
Thiago-Caffaro <96150887+Thiago-Caffaro@users.noreply.github.com>
2023-09-07 15:08:56 -0300

    Javascript Linkado

--- paginas/script.js
+++ paginas/script.js
@@ -10,15 +10,18 @@ const dataNasc = new Date(2007, 0, 18);
 var boa = document.querySelector("#boa");
 var idade = document.querySelector("#idade");
 
+console.log(hora);
 
 if (hora < 12){
     boa.innerHTML = "Bom dia!"
+
 }    
 else if (hora > 12 && hora < 18){
     boa.innerHTML = "Boa tarde!"
 }
 else if (hora > 18 || hora == 18 && min > 0){
     boa.innerHTML = "Boa noite!"
+
 }
 const miliDiff = data - dataNasc;
 const idadeNum = Math.floor(miliDiff / (1000 * 60 * 60 * 24 * 365.25));
